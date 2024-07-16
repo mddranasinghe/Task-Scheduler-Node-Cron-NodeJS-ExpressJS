d2d9c24f9a6b20c9bdd487498973dcd77a764455
Dinuka Dulanjana Ranasinghe <128118056+mddranasinghe@users.noreply.github.com>
2024-07-16 21:47:58 +0530

    up

--- scene/index.js
+++ scene/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+//const express = require('express');
+//const bodyParser = require('body-parser');
 const cron = require('node-cron');
 const fs = require('fs-extra');
 const path = require('path');
-const cors = require('cors'); 
+//const cors = require('cors'); 
 const mqtt = require('mqtt'); 
 
-const app = express();
+/*const app = express();
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors()); */
 
 const jobs = {};
 const top = 'v2jlcwytpqaufx/scene/#';
